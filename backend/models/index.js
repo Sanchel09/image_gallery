6492e89746e6b147c2041f9a4327f9516ad32395
Sandeep Shakya <sandeepshakya27@gmail.com>
2026-05-13 15:37:40 +0545

    Fixed models issue

--- backend/models/index.js
+++ backend/models/index.js
@@ -11,7 +11,9 @@ const __dirname = path.dirname(__filename);
 const basename = path.basename(__filename);
 
 // ✅ FIX: replace require with dynamic import
-import configFile from "../config/config.json" assert { type: "json" };
+const configFile = JSON.parse(
+  fs.readFileSync(new URL("../config/config.json", import.meta.url)),
+);
 
 const env = process.env.NODE_ENV || "development";
 const config = configFile[env];
